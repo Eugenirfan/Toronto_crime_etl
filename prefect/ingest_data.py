b1a88e60038610d0e48dbb37b933263bc6df9b86
Irfan <46944817+Eugenirfan@users.noreply.github.com>
2023-04-12 03:35:18 -0400

    Update ingest_data.py

--- prefect/ingest_data.py
+++ prefect/ingest_data.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 import pandas as pd
+import requests
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
+from prefect_gcp.cloud_storage import GcsBucket
 from random import randint
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 
 @task(retries=3)
@@ -11,45 +15,52 @@ def fetch(dataset_url: str) -> pd.DataFrame:
     # if randint(0, 1) > 0:
     #     raise Exception
 
-    df = pd.read_csv(dataset_url)
-    return df
+    crime_df = pd.read_csv(dataset_url)
+    return crime_df
 
 
 @task(log_prints=True)
-def clean(df: pd.DataFrame) -> pd.DataFrame:
+def clean(crime_df: pd.DataFrame) -> pd.DataFrame:
     """remove unwanted columns"""
-    df = df.drop(columns=['HOOD_140', 'NEIGHBOURHOOD_140'])
-    print(df.head(2))
-    print(f"columns: {df.dtypes}")
-    print(f"rows: {len(df)}")
-    return df
+    crime_df = crime_df.drop(columns=['HOOD_140', 'NEIGHBOURHOOD_140'])
+    print(crime_df.head(2))
+    print(f"columns: {crime_df.dtypes}")
+    print(f"rows: {len(crime_df)}")
+    return crime_df
 
+
+# @task()
+# def write_local(dataset_url: str) -> Path:
+#     """Write DataFrame out locally as csv file"""
+#     path = Path(f"data.csv")
+#     df.to_csv(path)
+#     return path
 
 @task()
-def write_local(df: pd.DataFrame) -> Path:
-    """Write DataFrame out locally as csv file"""
-    path = Path(f"data.csv")
-    df.to_csv(path)
-    return path
-
-@task()
-def write_gcs(path: Path) -> None:
+def write_gcs(crime_df: pd.DataFrame) -> None:
     """Upload local parquet file to GCS"""
-    gcs_block = GcsBucket.load("zoomcampfinal")
-    gcs_block.upload_from_path(from_path=path, to_path=path)
+    gcs_bucket = GcsBucket.load("zoomcampfinal")
+    gcs_bucket.upload_from_dataframe(df=crime_df,to_path="data.csv", serialization_format='csv')
     return
 
 
 @flow()
 def etl_web_to_gcs() -> None:
     """The main ETL function"""
+    base_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca"
+ 
+    # Datasets are called "packages". Each package can contain many "resources"
+    # To retrieve the metadata for this package and its resources, use the package name in this page's URL:
+    url = base_url + "/api/3/action/package_show"
+    params = { "id": "major-crime-indicators"}
+    package = requests.get(url, params = params).json()
+    temp_url = package['result']['resources'][3]['url']
 
-    dataset_url = f"major-crime-indicators.csv"
+    dataset_url = temp_url
 
     df = fetch(dataset_url)
     df_clean = clean(df)
-    path = write_local(df_clean)
-    write_gcs(path)
+    write_gcs(df_clean)
 
 
 
